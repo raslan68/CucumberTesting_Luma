33df71d7c53e4aa426782a29270fe7810f0a3998
cybertek <github@cybertekschool.com>
2020-02-25 14:04:35 -0500

    first scenario

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,7 +1,49 @@
 package com.bookit.step_definitions;
 
+import com.bookit.utilities.BookitAPIUtils;
+import com.bookit.utilities.ConfigurationReader;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import static io.restassured.RestAssured.*;
 public class ApiStepDefs {
-	
+
+
+    String token;
+    Response response;
+    @Given("I logged Bookit api using {string} and {string}")
+    public void i_logged_Bookit_api_using_and(String email, String password) {
+
+        token = BookitAPIUtils.generateToken(email,password);
+
+    }
+
+    @When("I get the current user information from api")
+    public void i_get_the_current_user_information_from_api() {
+
+        //url for api
+       String url = ConfigurationReader.get("qa1api.uri")+"/api/users/me";
+
+       response = given().accept(ContentType.JSON)
+                .header("Authorization", token)
+                .when().get(url);
+
+
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(int statusCode) {
+
+        //verify status code is matching with feature file
+        Assert.assertEquals(response.statusCode(),statusCode);
+
+    }
+
 
 
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -1,5 +1,6 @@
 Feature: User Verification
 
+  @wip
   Scenario: verify information about logged user
     Given I logged Bookit api using "sbirdbj@fc2.com" and "asenorval"
     When I get the current user information from api
